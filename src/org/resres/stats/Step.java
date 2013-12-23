661958ca4e2430efc06c99c7fed0125b9a989abc
Steve Doubleday <stevedoubleday@gmail.com>
2013-12-23 10:36:14 -0800

    steve:  comments on Step

--- src/org/resres/stats/Step.java
+++ src/org/resres/stats/Step.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public interface Step
 {
-
+	//TODO consider whether execute should return result directly
 	public void execute();
 
 	public String explain();
 
 	/**
-	 * Returns null prior to execution of the command.  
-	 * Once the command has been executed, value of result is defined by implementing Commands.s
+	 * Returns null prior to execution of the step.  
+	 * Once the step has been executed, value of result is defined by the implementation Step
 	 * @return
 	 */
 	public Double getResult();
